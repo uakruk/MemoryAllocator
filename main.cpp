f83dbd30ccaf1baef098ab402439de33266caef1
uakruk <uakruk@ukr.net>
2016-04-27 15:24:03 +0300

    for lab

--- main.cpp
+++ main.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 int main() {
-    int * i;
-    long * l;
+    int * i, *a, *b ,*c, *d;
+    long * l, *z, *x, *n, *m;
+    void * addr1, *addr2, *addr3, *addr4, *addr5;
+
     cout << "Hello, World!" << endl;
-    init(malloc(2000), 1500);
+    init(malloc(2000), 2000);
     cout << "sizeof(struct block) " << sizeof(struct block) << endl;
     cout << "size_t " << sizeof(size_t) << endl;
     cout << "sizeof(header_block) " << sizeof(block_header) << endl;
@@ -18,19 +20,20 @@ int main() {
     cout << last_free << endl;
     cout << (i = (int*)mem_alloc(16)) << endl;
     mem_free(i);
-    cout << mem_alloc(20) << endl;
+    cout << (addr1 = mem_alloc(20)) << endl;
     mem_dump(i);
-    cout << mem_alloc(16) << endl;
+    cout << (addr2 = mem_alloc(50)) << endl;
+    mem_free(addr1);
+    cout << (a = (int*)mem_alloc(16)) << endl;
     mem_dump(i);
     cout << mem_alloc(7) << endl;
     mem_dump(i);
-    cout << mem_alloc(7) << endl;
-    mem_dump(i);
-    cout << mem_alloc(10) << endl;
+    cout << (a = (int*)mem_realloc(a, sizeof(int))) << endl;
     mem_dump(i);
     cout << (l = (long*)mem_alloc(sizeof(long))) << endl;
     mem_dump(i);
     cout << mem_alloc(20) << endl;
+    mem_free(a);
     mem_dump(i);
     cout << mem_alloc(30) << endl;
     mem_dump(i);
@@ -38,12 +41,13 @@ int main() {
     mem_dump(i);
     cout << mem_alloc(40) << endl;
     mem_dump(i);
-//    cout << mem_alloc(8) << endl;
+    cout << (addr3 = mem_alloc(13)) << endl;
     mem_dump(i);
     cout << mem_alloc(8) << endl;
     mem_dump(i);
     cout << mem_alloc(50) << endl;
     mem_dump(i);
+    mem_free(addr3);
     cout << mem_alloc(50) << endl;
     mem_dump(i);
     return 0;
